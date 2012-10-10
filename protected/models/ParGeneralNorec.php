6c5ab00a953107186a2b6169a4a01d1b093895e8
Sharon Lavie <sharon@inflow.co.il>
2012-10-10 12:27:22 +0200

    parGeneralRec
    the MVC basic, before the changes was made

--- protected/models/ParGeneralNorec.php
+++ protected/models/ParGeneralNorec.php
@@ -1,19 +1,21 @@
 <?php
 
 /**
- * This is the model class for table "par_general".
+ * This is the model class for table "par_general_norec".
  *
- * The followings are the available columns in table 'par_general':
- * @property string $param
- * @property string $sub_param
- * @property integer $param_value
- * @property string $param_text
+ * The followings are the available columns in table 'par_general_norec':
+ * @property string $param_name
+ * @property integer $param_id
+ * @property string $param_value
+ * @property string $start_date
+ * @property string $end_date
  */
-class General extends CActiveRecord
+class ParGeneralNorec extends CActiveRecord
 {
 	/**
 	 * Returns the static model of the specified AR class.
-	 * @return General the static model class
+	 * @param string $className active record class name.
+	 * @return ParGeneralNorec the static model class
 	 */
 	public static function model($className=__CLASS__)
 	{
@@ -25,7 +27,7 @@ class General extends CActiveRecord
 	 */
 	public function tableName()
 	{
-		return 'par_general';
+		return 'par_general_norec';
 	}
 
 	/**
@@ -36,11 +38,13 @@ class General extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('param_value', 'numerical', 'integerOnly'=>true),
-			array('param, sub_param, param_text', 'length', 'max'=>50),
+			array('param_name, param_value', 'required'),
+			array('param_id', 'numerical', 'integerOnly'=>true),
+			array('param_name, param_value', 'length', 'max'=>50),
+			array('start_date, end_date', 'safe'),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('param, sub_param, param_value, param_text', 'safe', 'on'=>'search'),
+			array('param_name, param_id, param_value, start_date, end_date', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -61,10 +65,11 @@ class General extends CActiveRecord
 	public function attributeLabels()
 	{
 		return array(
-			'param' => 'Param',
-			'sub_param' => 'Sub Param',
+			'param_name' => 'Param Name',
+			'param_id' => 'Param',
 			'param_value' => 'Param Value',
-			'param_text' => 'Param Text',
+			'start_date' => 'Start Date',
+			'end_date' => 'End Date',
 		);
 	}
 
@@ -79,13 +84,16 @@ class General extends CActiveRecord
 
 		$criteria=new CDbCriteria;
 
-		$criteria->compare('param',$this->param,true);
-		$criteria->compare('sub_param',$this->sub_param,true);
-		$criteria->compare('param_value',$this->param_value);
-		$criteria->compare('param_text',$this->param_text,true);
+		$criteria->compare('param_name',$this->param_name,true);
+		$criteria->compare('param_id',$this->param_id);
+		$criteria->compare('param_value',$this->param_value,true);
+		$criteria->compare('start_date',$this->start_date,true);
+		$criteria->compare('end_date',$this->end_date,true);
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
 		));
 	}
+	
+	
 }
